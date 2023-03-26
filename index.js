26141a4c2baefa99b46a1f023b07d9927d257558
James Hawk <jph94880@gmail.com>
2023-03-26 15:26:33 -0500

    Adds my file.md

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function wrapAdjective(highLight = "*") {
+    return function(adjective) {
+        return (`You are ${highLight}${adjective}${highLight}!`);
+    }
+}
+
+
+
+
+console.log(mondayWork());
+console.log(saturdayFun('bathe my dog'));
+
+console.log(wrapAdjective()('a hard worker'));
+console.log(wrapAdjective("||")('a hard worker'));
